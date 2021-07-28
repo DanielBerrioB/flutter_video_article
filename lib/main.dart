778754271ab94bb79a6136165aad9013569cb985
Daniel Berrío Barrera <daniberrio10@hotmail.com>
2021-07-27 19:13:24 -0500

    Feat(Upload): First commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// @packages
+import 'package:flutter/material.dart';
+
+// @widgets';
+import 'package:flutter_video_article/video_player/tabs_video_player/video_tabs.dart';
+import 'package:flutter_video_article/video_player/tabs_video_player/video_tabs_no_state.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final url =
+      'https://assets.mixkit.co/videos/preview/mixkit-meadow-surrounded-by-trees-on-a-sunny-afternoon-40647-large.mp4';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom video player',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: VideoTabsNoState(),
+    );
+  }
+}
